8082227fd28945d41a8ba36af36315dfacce38bb
evgluzedev <evgeniy.shostak.04@mail.ru>
2025-03-13 23:48:34 +0300

    refactoring

--- src/main/java/web/forum/imageservice/config/ApplicationConfig.java
+++ src/main/java/web/forum/imageservice/config/ApplicationConfig.java
@@ -1,18 +1,16 @@
 package web.forum.imageservice.config;
 
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.cache.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.data.redis.cache.*;
 import org.springframework.data.redis.connection.*;
-import org.springframework.data.redis.connection.lettuce.*;
+import org.springframework.data.redis.connection.jedis.*;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.*;
 
 import java.time.*;
 
 @Configuration
-@EnableCaching
 public class ApplicationConfig {
 
     @Value("${spring.data.redis.host}")
@@ -21,16 +19,20 @@ public class ApplicationConfig {
     @Value("${spring.data.redis.port}")
     private Integer port;
 
-    @Bean
-    public RedisConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
-    }
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json()));
+    }
+
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
     @Bean
     public RedisTemplate<Long, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
